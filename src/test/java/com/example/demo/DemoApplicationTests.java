7fc87f59b455e9355ec3bec4ad8c72f5a350a184
Lance <lwangxiangyi@gmail.com>
2019-02-22 14:07:36 +0800

    jdbc

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,16 +1,24 @@
 package com.example.demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Slf4j
 public class DemoApplicationTests {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     @Test
     public void contextLoads() {
+
+        log.info("==============================");
     }
 
 }
