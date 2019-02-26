5c5069e19e89b99ee984679fff1c1cf4c3ec749f
Lance <lwangxiangyi@gmail.com>
2019-02-26 15:55:31 +0800

    jpa.test2

--- src/main/java/com/example/demo/User.java
+++ src/main/java/com/example/demo/User.java
@@ -2,19 +2,19 @@ package com.example.demo;
 
 import lombok.Data;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "user")
 @Data
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column
     private String username;
     @Column
     private String password;
+    @Transient
+    private String create_time;
 }
