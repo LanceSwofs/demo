c80ae77fd014f9b8d287ede80be3a9e9ce740a06
Lance <lwangxiangyi@gmail.com>
2019-02-27 17:29:16 +0800

    amqp.test1

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -14,14 +15,18 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class DemoApplicationTests {
     @Autowired
     private JdbcTemplate jdbcTemplate;
-
+    @Autowired
+    private AmqpTemplate amqpTemplate;
     @Autowired
     private UserRepository userRepository;
 
     @Test
     public void contextLoads() {
-        User test = userRepository.findByUsername("test");
-        log.info("=============================={}", test);
+        User user = new User();
+        user.setUsername("lixiao");
+        User save = userRepository.save(user);
+        log.info("=============================={}", save);
+        amqpTemplate.convertAndSend("Hello World!") ;
     }
 
 }
